7fdc3bfecef5f2b7faa404932ac9a0bc2d2a87d4
KeisLuv5991 <chmhj9997@gmail.com>
2022-09-15 14:49:41 +0900

    fix build script

--- python_lambdas/plant_detection/build.sh
+++ python_lambdas/plant_detection/build.sh
@@ -1,3 +1,4 @@
+aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 305106164225.dkr.ecr.ap-northeast-2.amazonaws.com
 docker build -t plant_detection_x86 . --platform=linux/amd64
 docker tag plant_detection_x86 305106164225.dkr.ecr.ap-northeast-2.amazonaws.com/lambda-plant-detection
 docker push 305106164225.dkr.ecr.ap-northeast-2.amazonaws.com/lambda-plant-detection
