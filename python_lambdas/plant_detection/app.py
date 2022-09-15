df508207f96d0fe32a64e9f38b1aed7d5fc48ae7
KeisLuv5991 <chmhj9997@gmail.com>
2022-09-15 14:10:58 +0900

    add plant-detection

---
+++ python_lambdas/plant_detection/app.py
@@ -0,0 +1,41 @@
+import yolov5
+import json
+
+# load pretrained model
+model = yolov5.load('./plant_detection_weights.pt')
+
+# set model parameters
+model.conf = 0.25  # NMS confidence threshold
+model.iou = 0.45  # NMS IoU threshold
+model.agnostic = False  # NMS class-agnostic
+model.multi_label = False  # NMS multiple labels per box
+model.max_det = 1000  # maximum number of detections per image
+
+
+def handler(event, context):
+    
+    body = json.loads(event['body'])
+    
+    image_url = body['image_url']
+
+    # perform inference
+    results = model(image_url)
+
+    # parse results
+    predictions = results.pred[0]
+    boxes = predictions[:, :4]  # x1, y1, x2, y2
+    scores = predictions[:, 4]
+    categories = predictions[:, 5]
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps({
+            'image_url': image_url,
+            'predictions': json.dumps(predictions),
+        })
+    }
+
+
+print(handler({'body': '{"image_url": "https://blog.kakaocdn.net/dn/raSZ5/btqUTmCm2Ad/ZK2k13W5zeXjz0oTFoNSzk/img.png"}'}, None))
+    
+
